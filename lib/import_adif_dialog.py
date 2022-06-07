a676b054e65deee78221ee9c76d7e59e67652c57
Jakub Ondrusek <yak@gmx.co.uk>
2022-06-07 23:19:54 +0200

    import adif: qso variables + custom session, ugly but working

--- lib/import_adif_dialog.py
+++ lib/import_adif_dialog.py
@@ -31,6 +31,23 @@ class ImportAdifDialog(Gtk.Dialog):
         self.qso_variables = QsoVariablesEditor(parent.config)
         box.pack_start(self.qso_variables, True, True, 0)
 
+
+        self.session = {}
+        label_session_desc = Gtk.Label()
+        label_session_desc.set_markup("<b>Description (short):</b>")
+        self.session['description'] = Gtk.Entry(max_width_chars=64, width_chars=64)
+
+        label_session_note = Gtk.Label()
+        label_session_note.set_markup("<b>About Session (Note):</b>")
+        self.session['text_note'] = Gtk.TextView()
+
+        box.pack_start(label_session_desc, False, False, 0)
+        box.pack_start(self.session['description'], True, False, 0)
+
+        box.pack_start(label_session_note, False, False, 0)
+        box.pack_start(self.session['text_note'] , True, False, 0)
+
+
         self.import_button = Gtk.Button(label="Import")
         self.import_button.connect("button-press-event", self.run_import)
         box.pack_start(self.import_button, True, False, 0)
@@ -41,6 +58,16 @@ class ImportAdifDialog(Gtk.Dialog):
 
         self.header, self.qsos = read_file(self.input_file)
         self.qsos = process_qsos(self.header, self.qsos, custom_variables=self.qso_variables.value)
+
+        session_desc = self.session['description'].get_text()
+
+        buf = self.session['text_note'].get_buffer()
+        session_note = buf.get_text(*buf.get_bounds(),include_hidden_chars=False)
+
+        qso_session = self.parent_app.db.create_qso_session(
+            description=session_desc,
+            text_note=session_note
+        )
 
         for q in self.qsos:
             qso_variables = {}
@@ -64,7 +91,8 @@ class ImportAdifDialog(Gtk.Dialog):
                 rst_sent=q['RST_SENT'],
                 rst_received=q['RST_RCVD'],
                 text_note=q.get('COMMENT',''),
-                variables=qso_variables
+                variables=qso_variables,
+                qso_session = qso_session
             )
 
         self.parent_app.tree_data_refresh_main_tree()
